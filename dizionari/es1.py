f33ed944982f77cd3275e0bc05c54a25765e1cb3
Canecus45 <149671604+Canecus45@users.noreply.github.com>
2023-12-23 14:01:26 +0100

    Commit

--- dizionari/es1.py
+++ dizionari/es1.py
@@ -1,2 +1,22 @@
-voti={'italiano':7,'storia':5,'tpsi':6}
-print(voti)
+voti={}
+
+while True:
+    nChiave=input("Inserire la materia (se si inserisce fine il ciclo finisce): ")
+    if nChiave=="fine":
+        break
+    nVoto=int(input(f"Inserire il voto di {nChiave}: "))    
+    voti[nChiave]=nVoto
+
+cont=0
+
+for materia in voti:
+    print(f"-{materia}: {voti[materia]}")
+    cont+=1
+
+media=0
+
+for materia in voti:
+    media+=voti[materia]
+
+media=media/cont
+print(f"La media è {media}")
